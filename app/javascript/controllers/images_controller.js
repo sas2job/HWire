96b351fbe6026bb4d6d6797857dffb76b3eabb37
Александр Смирнов <sas2job@gmail.com>
2026-02-07 19:16:01 +0300

    Add toast notification for copy action

--- app/javascript/controllers/images_controller.js
+++ app/javascript/controllers/images_controller.js
@@ -1,11 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
-
+import { Toast } from "bootstrap"
 export default class extends Controller {
   connect(){
     console.log('connected!')
     console.log(this.element)
+    this.toast = new Toast(
+      document.getElementById('copyToast')
+    )
   }
   getUrl(e) {
     navigator.clipboard.writeText(e.target.src)
+    this.toast.show
   }
 }
