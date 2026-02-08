aa101a77168bfd2b9504ba57fd98d2e14c7ccd3c
Александр Смирнов <sas2job@gmail.com>
2026-02-08 21:01:14 +0300

    Editable elements

--- app/javascript/controllers/images_controller.js
+++ app/javascript/controllers/images_controller.js
@@ -1,11 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
-  static targets = ['image']
+  static targets = ['image', 'title', 'save']
 
   connect(){
     const title = document.createElement('p')
     title.textContent = this.imageTarget.alt
+    title.contentEditable = true
+    title.dataset.imagesTarget = 'title'
+    title.dataset.action = 'click->images#editTitle'
     this.element.appendChild(title)
+  }
+
+  editTitle(e){
+    if(!this.hasSaveTarget) {
+      const btn = document.createElement('button')
+      btn.textContent = 'Save'
+      btn.classList = 'btn btn-primary btn-sm'
+      btn.dataset.imagesTarget = 'save'
+      btn.dataset.action = 'click->images#saveTitle'
+      e.target.insertAdjcentElement('afterend', btn)
+    }
+  }
+
+  saveTitle(e){
+    e.preventDefault()
+    e.target.disabled = true
   }
 
   getUrl(e) {
@@ -16,4 +35,8 @@ export default class extends Controller {
   // imageTargetConnected(element) {
   //   console.log(element)
   // }
+
+  titleTargetConnected(element) {
+    console.log(element)
+  }
 }
