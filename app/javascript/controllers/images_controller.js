8b588b59e1f06c0f7996d876b22dc81690acf26f
Александр Смирнов <sas2job@gmail.com>
2026-02-08 19:45:56 +0300

    Creating elements on the fly and using targets

--- app/javascript/controllers/images_controller.js
+++ app/javascript/controllers/images_controller.js
@@ -1,10 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
-import { Toast } from "bootstrap"
 export default class extends Controller {
+  static targets = ['image']
+
   connect(){
+    const title = document.createElement('p')
+    title.textContent = this.imageTarget.alt
+    this.element.appendChild(title)
   }
+
   getUrl(e) {
     navigator.clipboard.writeText(e.target.src)
     this.dispatch('copy', { detail: { content: 'Image URL has been copied!'}})
   }
+
+  // imageTargetConnected(element) {
+  //   console.log(element)
+  // }
 }
