984cc5e6a3f5bcefe5c4e8d3df42d4dac3a93d3a
Александр Смирнов <sas2job@gmail.com>
2026-02-10 11:00:19 +0300

    Create new records with turbo_frames use replace

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,7 +1,21 @@
 class FoldersController < ApplicationController
   before_action :set_folder!, only: %i[edit update]
+
   def index
     @folders = Folder.order created_at: :desc
+  end
+
+  def new
+    @folder = Folder.new
+  end
+
+  def create
+    @folder = Folder.new folders_params
+    if @folder.save
+      # redirect_to folders_path
+    else
+      render :new
+    end
   end
 
   def edit; end
